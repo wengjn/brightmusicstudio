10a5bcb79b24ca63cce60164145af548f7465bc3
jiannian weng <jiannianweng@jiannians-Air.home>
2020-03-26 18:17:54 -0700

    Add bower.json

---
+++ build/precache-manifest.ab45e112dd9da61ed1da717dab03eaf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bd18f5a958f73fc518e7a0098e46c12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0e9693bb9edc865d0f2",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "21f09a51d79e3dd7cdd6",
+    "url": "/static/js/2.eb499f6b.chunk.js"
+  },
+  {
+    "revision": "bc51ad6b075e5b96cf9ad2ad3d22ac0d",
+    "url": "/static/js/2.eb499f6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0e9693bb9edc865d0f2",
+    "url": "/static/js/main.1a9ac7d6.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
